d465c90bfd3cce0da025adf1fa9204fa8a64993c
vidigaljr <ivan.vidigaljr@gmail.com>
2022-09-18 20:00:48 -0300

    projeto angular eletriza

--- src/app/cadastrados/cadastro-services/cadastros.service.ts
+++ src/app/cadastrados/cadastro-services/cadastros.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { CadastroModel } from '../cadastro-model/cadastro.model';
+import { CadastroComponent } from 'src/app/Home/cadastro/cadastro.component';
+import { CadastradosComponent } from '../cadastrados.component';
 
 
 @Injectable({
@@ -12,18 +14,22 @@ export class CadastrosService {
   constructor(private http: HttpClient) {
 }
   cadastrarContato(contato: CadastroModel): Observable<any>{
-  return this.http.post("http://localhost:4000/cadastrados/", contato)
+  return this.http
+  .post("http://localhost:3000/cadastrados/", contato)
  }
 
   listarCadastro(): Observable<any>{
-    return this.http.get("http://localhost:4000/cadastrados");
+    return this.http
+    .get("http://localhost:3000/cadastrados");
   }
   atualizarCadastro(id:any, contato: CadastroModel): Observable<any>{
-    return this.http.put("http://localhost:4000/cadastrados/".concat(id), contato);
+    return this.http
+    .put("http://localhost:3000/cadastrados/".concat(id), contato);
   }
 
   removerCadastro(id: any): Observable<any>{
-    return this.http.delete("http://localhost:4000/cadastrados/".concat(id));
+    return this.http
+    .delete("http://localhost:3000/cadastrados/".concat(id));
   }
 }
 
